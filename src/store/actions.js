7b7b6c7371d0ee0876f406ec5e9cbd22720d1352
summermacarole <1740249919@qq.com>
2018-05-11 00:16:08 +0800

    终极方案

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,7 +4,7 @@ export default {
   async getBase({commit, state, dispatch}, params) {
     console.log('获取基地址')
     let res = await  axios.get('http://localhost:3000/base')
-    commit('updateBase', res['data']['url'])
+    // commit('updateBase', res['data']['url']) 不保存在state里面就好
     return res;
   },
   async getBaseAbout({commit, state}, params) {
